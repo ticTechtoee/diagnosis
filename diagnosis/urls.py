732fbfa6f784d4d2123c7189dd45d1018d4318f9
Farhan Basheer <farhanfb6@outlook.com>
2024-06-04 00:30:12 +0500

    updated file extensions

--- diagnosis/urls.py
+++ diagnosis/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("AccountApp.urls")),
-    path('home/', include("HomeApp.urls")),
+    path('account/', include("AccountApp.urls")),
+    path('', include("HomeApp.urls")),
 ]
